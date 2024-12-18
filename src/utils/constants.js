93b215e2017bdcc9cf34d79ba8433b0e2f8c8ee2
kamranhafeezbim <kamran.hafeez@bigimmersive.com>
2024-12-18 18:17:35 +0500

    updates

---
+++ src/utils/constants.js
@@ -0,0 +1,10 @@
+export const stakingData = {
+    ikigai : {
+        mintAddr: "84AYw2XZ5HcyWWmVNR6s4uS3baHrMLpPMnEfBTm6JkdE",
+        stakeProg:"A1KnSRkwF34piFfioJ1NPfeCVRLp5rgJ6kjQz579LCqP",
+    },
+    tyke: {
+        mintAddr: "84AYw2XZ5HcyWWmVNR6s4uS3baHrMLpPMnEfBTm6JkdE",
+        stakeProg:"Apro8whm5cp1ruxpj8zDNTWmm8jLME3zjaV8tG8fPMfV",
+    }
+}
