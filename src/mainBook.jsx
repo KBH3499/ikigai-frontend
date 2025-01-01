b4a1b3e96527b5b49cd8ffe19415fc6039de7ade
kamranhafeezbim <kamran.hafeez@bigimmersive.com>
2025-01-01 14:01:38 +0500

    fix

--- src/mainBook.jsx
+++ src/mainBook.jsx
@@ -114,7 +114,7 @@ const MainBook = () => {
   const [isMobile, setIsMobile] = useState(false);
   const nextButtonClick = () => {
     const nextPageIndex = flipBook.current.pageFlip().getCurrentPageIndex() + 1;
-    if ((nextPageIndex === 61 || (isMobile && nextPageIndex === 62)) && !isAdminPanelEnabled) {
+    if ((nextPageIndex === 61 || (isMobile && nextPageIndex === 60)) && !isAdminPanelEnabled) {
       return;
     }
     flipBook.current.pageFlip().flipNext();
@@ -232,6 +232,7 @@ const MainBook = () => {
   ]
 
   const handleFlip = (e) => {
+    console.log(e.data)
     setCurrentVisiblePage(e.data); // Update the current visible page
   };
 
