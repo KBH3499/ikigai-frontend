3fc3bdc63e09cfba897f836a47c94c7bb513eabc
kamranhafeezbim <kamran.hafeez@bigimmersive.com>
2025-01-01 13:33:05 +0500

    fix

--- src/pages/music-section/MusicPageLeft.jsx
+++ src/pages/music-section/MusicPageLeft.jsx
@@ -2,17 +2,17 @@ import React from "react";
 import { useMusicPlayer } from "../../provider/music-provider";
 import musicList from "../../json/music-list.json"
 const MusicPageLeft = React.forwardRef((props, ref) => {
-   const {songIndex}=useMusicPlayer();
+  const { songIndex } = useMusicPlayer();
   return (
     <div className="demoPage comic_background_white_left music_page_container" ref={ref}>
-         <h2 className="playlist-title" style={{marginTop:"20%"}}>Music</h2>
-        <div className="music-item">
-        <img src={`/music-assets/${musicList[songIndex].imgSrc}`} style={{height:'60%',width:'60%'}} alt="Song Artwork" />
-        </div>
-        <div className="description">
-            <p>{musicList[songIndex].description}</p>
-        </div>
-        </div>
+      <h2 className="playlist-title" style={{ marginTop: "20%" }}>Music</h2>
+      <div className="music-item">
+        <img src={`/music-assets/${musicList[songIndex].imgSrc}`} style={{ height: 'auto', width: '50%', marginTop: -45, marginBottom: -40 }} alt="Song Artwork" />
+      </div>
+      <div className="description">
+        <p>{musicList[songIndex].description}</p>
+      </div>
+    </div>
   );
 });
 
